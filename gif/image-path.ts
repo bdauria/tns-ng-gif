eabe2787c9943cda2f8263d0c3f07b179835daa2
Bruno <bruno.dauria1@gmail.com>
2017-03-27 20:23:22 +0200

    Remove unused image-path.ts file.

--- gif/image-path.ts
+++
@@ -1,9 +0,0 @@
-import { Image } from 'ui/image';
-import { path, knownFolders } from 'file-system';
-
-export const absolutePathOf = (image: Image) : string => {
-    return path.join(
-        knownFolders.currentApp().path,
-        image.src.replace("~/", "")
-    );
-}
