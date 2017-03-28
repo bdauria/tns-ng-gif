f675add769a40147a1b5ee1ccab4f4797c73ef07
Bruno <bruno.dauria1@gmail.com>
2017-03-28 19:46:37 +0200

    Replace double quotes with single ones.

--- gif/gif.directive.ts
+++ gif/gif.directive.ts
@@ -5,7 +5,7 @@ import {
 } from '@angular/core';
 import { path, knownFolders } from 'file-system';
 import { Image } from 'ui/image';
-import { toGif } from "./image-to-gif";
+import { toGif } from './image-to-gif';
 
 @Directive({
   selector: '[tnsGif]'
