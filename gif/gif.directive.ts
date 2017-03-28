2e9746b15048c3565dca933801b2ee0574741aa8
Bruno <bruno.dauria1@gmail.com>
2017-03-28 19:44:28 +0200

    Wrap GifDirective implementation in loaded event instead of NgOnInit.

--- gif/gif.directive.ts
+++ gif/gif.directive.ts
@@ -1,7 +1,7 @@
 import {
   Directive,
   ElementRef,
-  OnInit
+  HostListener
 } from '@angular/core';
 import { path, knownFolders } from 'file-system';
 import { Image } from 'ui/image';
@@ -10,10 +10,10 @@ import { toGif } from "./image-to-gif";
 @Directive({
   selector: '[tnsGif]'
 })
-export class GifDirective implements OnInit {
+export class GifDirective {
   constructor(private el: ElementRef) { }
 
-  public ngOnInit(): void {
+  @HostListener('loaded') onLoaded() {
     const nativeElement = this.el.nativeElement;
     if (!(nativeElement instanceof Image)) {
       throw new Error('[tnsGif] must be applied to an Image');
